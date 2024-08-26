a4774ce7e86e3f019b71b0b799c5a534e6639626
dzakwan <dzakwandma@gmail.com>
2024-08-26 15:04:52 +0700

    chore: Add meta tags for SEO optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>JCO Coffee - Premium Coffee and Donuts</title>
+        <meta
+          name="description"
+          content="Discover JCO's premium coffee and delicious donuts. Enjoy a wide variety of flavors and high-quality brews."
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
